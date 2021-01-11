df0aa963ca87ba4236581f421ee3e00c2fe4b791
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-11 11:17:56 -0500

    added fishCollection array of objects to js

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,11 @@
+let fishCollection = [
+  {
+      name: "Bart",
+      food: "Crustaceans",
+      type: "Beta Fish",
+      location: "Galapagos",
+      length: 2
+  }
+]
+
+console.log(fishCollection);
