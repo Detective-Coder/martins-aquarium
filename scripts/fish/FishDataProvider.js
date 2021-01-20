b5b9dea05c11a88d10eeb8c33b0c1aa86f01fc98
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-20 09:41:24 -0500

    created functions for holyfish, soldierfish, and nonholy fish

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -18,30 +18,77 @@ let fishCollection = [
       food: "Hotdogs",
       type: "Orange Fish",
       location: "Atlantic Ocean",
-      length: 8
+      length: 10
   },
   {
-    name: "Bart",
+    name: "Jeff",
     food: "Crustaceans",
     type: "Beta Fish",
     location: "Galapagos",
     length: 2
   },
   {
-      name: "Fred",
+      name: "Sam",
       food: "Other Fish",
       type: "Fishy Fish",
       location: "Hawaii",
       length: 9
   },
   {
-      name: "Wilma",
+      name: "Betty",
       food: "Hotdogs",
       type: "Orange Fish",
       location: "Atlantic Ocean",
       length: 8
   }
 ];
+
+export const mostHolyFish = () => {
+  // 3, 6, 9, 12, etc... fish
+  const holyFish = [];
+
+ // Only display multiples of 3
+  for (const fish of fishCollection) {
+    if (fish.length % 3 === 0) {
+        holyFish.push(fish);
+    }
+  }
+
+  return holyFish
+}
+
+export const soldierFish = () => {
+  // 5, 10, 15, 20, 25, etc... fish
+  const soldiers = [];
+
+  // Only display multiples of 5
+  for (const fish of fishCollection) {
+    if (fish.length % 5 === 0) {
+        soldiers.push(fish);
+    }
+  }
+
+  return soldiers
+}
+
+const roughFish = soldierFish();
+console.log(roughFish);
+
+export const nonHolyFish = () => {
+  // Any fish not a multiple of 3 or 5
+  const regularFish = [];
+
+  // Only display nonmultiples of 3 or 5
+  for (const fish of fishCollection) {
+    if (fish.length % 3 !== 0 && fish.length % 5 !== 0) {
+        regularFish.push(fish);
+    }
+  }
+
+  return regularFish
+
+}
+
 
 export const useFish = () => {
   return fishCollection.slice()
